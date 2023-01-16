e174b856338bdb5eaaf0536453b581dc0a374496
Justin Thomas <justin@jdt.io>
2023-01-16 11:30:41 -0800

    processing_queue

--- src/routes/api/user/[username]/processing_queue/+server.ts
+++ src/routes/api/user/[username]/processing_queue/+server.ts
@@ -5,7 +5,7 @@ import { json } from '@sveltejs/kit';
 export async function GET({ params }: RequestEvent) {
     const { username } = params
 
-  const resp = await fetch('http://192.168.37.221:8010/api/user/' + username, {
+  const resp = await fetch('http://192.168.37.221:8010/api/user/' + username + "/processing_queue", {
       method: 'GET',
       headers: {
           'Content-type': 'application/json; charset=UTF-8'
